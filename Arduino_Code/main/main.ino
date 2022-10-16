9c3713d103de17be6dcfb81571a5b2fe0d5d2469
Gorman Daniel Hammond <gorman@hubtel.com>
2022-10-16 16:56:16 +0000

    change logic

--- Arduino_Code/main/main.ino
+++ Arduino_Code/main/main.ino
@@ -6,25 +6,20 @@
 Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 LiquidCrystal_I2C lcd(0x27,16,2);
 Servo myservo;
-const int trigPin = 9;
-const int echoPin = 10;
+const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
+const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
 const int buzzer = 8;
 const float normal_temp = 37.2;
-const int relay_pin = 7;
-int sensor = 2;  //PIR data pin 
-long duration;
-int distance;
-int dis;
+const int relay_pin = 3;
+int sensor = 2;  //PIR data pin
 int val = 0;
+long measured_Distance;
 int inByte = '1';
 
 void setup() {   
     lcd.init();
     lcd.clear();         
-    lcd.backlight(); 
-    
-    pinMode(trigPin, OUTPUT); 
-    pinMode(echoPin, INPUT); 
+    lcd.backlight();   
     pinMode(buzzer, OUTPUT);
     pinMode(relay_pin, OUTPUT);
     pinMode(sensor, INPUT); 
@@ -43,9 +38,7 @@ void setup() {
 
 }
 
-void loop() {
-   if (Serial.available() > 0) 
-   {
+void loop() {   
       Serial.println("run"); 
       lcd.clear(); 
       lcd.backlight(); 
@@ -54,117 +47,121 @@ void loop() {
       delay(100); 
       noTone(buzzer); 
       lcd.print("Welcome to AIT");
-      dis = measureDistance();     
-      if(dis > 5){
-        //check noise mask         
-         lcd.clear(); 
-         lcd.setCursor(0,0); 
-         lcd.print("Please face the");   
-         lcd.setCursor(0,1); 
-         lcd.print("Camera");         
-         delay(1000);
-         Serial.println("start"); 
-         inByte = (Serial.read());         
-         if( inByte == '9'){
-            lcd.clear(); 
-            lcd.setCursor(0,0); 
-            lcd.print("Please face temp");
+      delay(2000);
+      measured_Distance = measureDistance();      
+      if(measured_Distance < 20){
+        int lock = 0;
+        clearLCD();
+        while(lock != 1) {
             delay(1000);
-            lcd.setCursor(0,1);  
-            lcd.print("sensor ..");
+            clearLCD();
+            lcd.print("Please face the");   
+            lcd.setCursor(0,1); 
+            lcd.print("Camera");         
             delay(1000);
-            lcd.clear(); 
-            lcd.setCursor(0,0); 
-            lcd.print("Please face temp");
-            float  ambient_Temp = mlx.readAmbientTempC();
-            float temp = mlx.readObjectTempC();
-            for (int i = 0; i <= 10; i++) {
-              lcd.clear(); 
-              lcd.setCursor(0,0); 
-              lcd.print("Measuring ....");
-              Serial.println("Measuring.....");
-              delay(100);
-              }
-              if( ambient_Temp > temp){
-                  lcd.clear(); 
-                  lcd.setCursor(0,0); 
-                  lcd.print("Face the sensor");
-                  lcd.setCursor(0,1);
-                  lcd.print("to measure again");
-                  delay(200);
-                  for (int i = 0; i <= 10; i++) {
-                    lcd.clear(); 
-                    lcd.setCursor(0,0); 
-                    lcd.print("Measuring ....");
-                    delay(100);
-                  }
-                  ambient_Temp = mlx.readAmbientTempC();
-                  temp = mlx.readObjectTempC();
-              }                     
-          lcd.clear(); 
-          lcd.setCursor(0,0); 
-          lcd.print("Your Tempature");
-          Serial.println("Your Tempature");
-          lcd.setCursor(0,10); 
-          lcd.print(temp);
-          delay(1000);
-          if(temp > normal_temp){
-              tone(buzzer, 1000);
-              lcd.clear(); 
-              lcd.setCursor(0,0); 
-              lcd.print("Temperature High");
-              Serial.println("Temperature High");
-              lcd.setCursor(0,1);
-              lcd.print("Contact Doctor");                 
-              delay(1000);
-              noTone(buzzer); 
-         }
-          lcd.clear(); 
-          lcd.setCursor(0,0); 
-          lcd.print("Please wash");
-          Serial.println("Please wash");
-          lcd.setCursor(0,1); 
-          lcd.print("your hands");
-          delay(1000);
-          val = digitalRead(sensor);   
-          if (val == HIGH) {           
+            Serial.println("start"); 
+            inByte = (Serial.read());    
+            if( inByte == '9'){
                 lcd.clear(); 
-                lcd.setCursor(0,0); 
-                lcd.print("Be Alert for ");
-                lcd.setCursor(0,1); 
-                lcd.print("the sanitizer");
+                lcd.print("Please face temp");
                 delay(1000);
-              }
-          myservo.write(180);              
-          delay(1500);
-      } 
-        else{
+                lcd.setCursor(0,1);  
+                lcd.print("sensor ..");
+                delay(1000);
+                lcd.clear(); 
+                lcd.print("Please face temp");
+                float  ambient_Temp = mlx.readAmbientTempC();
+                float temp = mlx.readObjectTempC();
+                for (int i = 0; i <= 10; i++){
+                    clearLCD();
+                    lcd.print("Measuring ....");
+                    Serial.println("Measuring.....");
+                    delay(100);                            
+                }
+                if( ambient_Temp > temp){
+                    clearLCD();
+                    lcd.print("Face the sensor");
+                    lcd.setCursor(0,1);
+                    lcd.print("to measure again");
+                    delay(200);
+                    for (int i = 0; i <= 10; i++) {
+                        clearLCD();
+                        lcd.print("Measuring ....");
+                        delay(100);
+                    }
+                    ambient_Temp = mlx.readAmbientTempC();
+                    temp = mlx.readObjectTempC();
+                }
+                clearLCD();
+                lcd.print("Your Tempature");
+                Serial.println("Your Tempature");
+                lcd.setCursor(0,10); 
+                lcd.print(temp);
+                delay(1000);
+                if(temp > normal_temp)
+                {
+                    tone(buzzer, 1000);
+                    clearLCD();
+                    lcd.print("Temperature High");
+                    Serial.println("Temperature High");
+                    lcd.setCursor(0,1);
+                    lcd.print("Contact Doctor");                 
+                    delay(1000);
+                    noTone(buzzer); 
+                    lock = 1;
+                    break;
+                }
+                clearLCD();
+                lcd.print("Please wash");
+                Serial.println("Please wash");
+                lcd.setCursor(0,1); 
+                lcd.print("your hands");
+                delay(1000);
+                val = digitalRead(sensor);   
+                if (val == HIGH) {           
+                    clearLCD();
+                    lcd.print("Be Alert for ");
+                    lcd.setCursor(0,1); 
+                    lcd.print("the sanitizer");
+                    delay(1000);
+                    myservo.write(180);                                
+                    delay(1500);
+                }
+            }
+            else{
             tone(buzzer, 1000);
-              lcd.clear(); 
-              lcd.setCursor(0,0); 
+              clearLCD();
               lcd.print("Wear Mask");
               Serial.println("Wear Mask");
               delay(1000);
               noTone(buzzer); 
-        }
-    }                 
-      delay(1000);
-      lcd.clear();
-       Serial.println("end");
-  
-  }
-
-}
+            }
+            lock = 1;  
+        }        
+      }  
+     delay(2000);
+     lcd.clear();
+    }
 
 
-int measureDistance (){  
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2); 
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);  
-  duration = pulseIn(echoPin, HIGH); 
-  distance = duration * 0.034 / 2;
+
+
+long measureDistance (){   
+  long duration, distance;
+   pinMode(pingPin, OUTPUT);
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(pingPin, LOW);
+   pinMode(echoPin, INPUT);
+   duration = pulseIn(echoPin, HIGH);
+   distance = duration / 29 / 2;
   Serial.println(distance);
   return distance;
+}
+
+void clearLCD(){
+  lcd.clear(); 
+  lcd.setCursor(0,0); 
 }
